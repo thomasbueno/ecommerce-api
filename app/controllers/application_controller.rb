52d1eae60bd35f578c3862f6e0214d009b9b0e0d
Thomás Escolástico Bueno <62290004+thomasbuenoU@users.noreply.github.com>
2020-11-02 18:48:28 -0300

    Allowing name parameter to sign up with devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::API
-        include DeviseTokenAuth::Concerns::SetUserByToken
+  before_action :configure_permitted_params, if: :devise_controller?
+  
+  protected
+  
+  def configure_permitted_params
+    devise_parameter_sanitizer.permit
+    (:sign_up, keys: [:name, :email, :password, :password_confirmation])
+  end
 end
