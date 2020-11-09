9d9842e4393683f26b9d8f969104945f71420e87
Thomás Escolástico Bueno <62290004+thomasbuenoU@users.noreply.github.com>
2020-11-09 19:42:42 -0300

    fix syntax of user factorie

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     name { Faker::Name.name }
-    email { Faker:Internet.email }
+    email { Faker::Internet.email }
     password { "123456" }
     password_confirmation { "123456" }
     profile { [:admin, :client].sample }
