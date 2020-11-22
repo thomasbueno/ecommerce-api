2fbcaee6efd26faf4bc153db54d05fe1729b3a9b
Thomás Escolástico Bueno <62290004+thomasbuenoU@users.noreply.github.com>
2020-11-22 02:54:39 -0300

    renaming file to admin_request_spec

--- spec/requests/admin/v1/system_requirements/admin_request_spec.rb
+++ spec/requests/admin/v1/system_requirements/admin_request_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Admin::V1::SystemRequirements", type: :request do
+RSpec.describe "Admin::V1::SystemRequirements as :admin", type: :request do
   let(:user) { create(:user) }
 
   context "GET /system_requirements" do
