f4c282e93827a86391458513b22fc02924614f26
Thomás Escolástico Bueno <62290004+thomasbuenoU@users.noreply.github.com>
2020-11-23 08:11:31 -0300

    creating create method to users with tests

--- app/controllers/admin/v1/users_controller.rb
+++ app/controllers/admin/v1/users_controller.rb
@@ -3,5 +3,27 @@ module Admin::V1
     def index
       @users = User.all
     end
+
+    def create
+      @user = User.new
+      @user.attributes = user_params
+
+      save_user!
+    end
+
+    private
+
+    def user_params
+      return {} unless params.key?(:user)
+      params.require(:user).permit(:name, :email, :profile, :password, :password_confirmation)
+    end
+
+    def save_user!
+      @user.save!
+      render :show
+
+    rescue
+      render_error(fields: @user.errors.messages)
+    end
   end
 end
