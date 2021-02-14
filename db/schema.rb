549ebec9dc18c8e7bbcae6d034d3590beb7d5f8b
Thomás Escolástico Bueno <62290004+thomasbuenoU@users.noreply.github.com>
2021-02-14 11:09:30 -0300

    add featured to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_12_164806) do
+ActiveRecord::Schema.define(version: 2021_02_14_140025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,6 +89,7 @@ ActiveRecord::Schema.define(version: 2021_01_12_164806) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "status"
+    t.boolean "featured", default: false
     t.index ["productable_type", "productable_id"], name: "index_products_on_productable_type_and_productable_id"
   end
 
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -9,6 +9,8 @@ RSpec.describe Product, type: :model do
   it { is_expected.to validate_presence_of(:description) }
   it { is_expected.to validate_presence_of(:price) }
   it { is_expected.to validate_numericality_of(:price).is_greater_than(0) }
+  it { is_expected.to validate_presence_of(:featured) }
+
 
   it { is_expected.to have_many(:product_categories).dependent(:destroy) }
   it { is_expected.to have_many(:categories).through(:product_categories) }
@@ -21,4 +23,10 @@ RSpec.describe Product, type: :model do
   
   it_has_behavior_of "like searchable concern", :product, :name
   it_behaves_like "paginatable concern", :product
+
+  it "creates as unfeatured by default" do
+    subject.featured = nil
+    subject.save(validate: false)
+    expect(subject.featured).to be_falsey
+  end
 end
