a5539b10615fe99fd6470594445191e04fc07a74
Thomás Bueno <62290004+thomasbueno@users.noreply.github.com>
2022-01-22 00:36:14 -0300

    creating validation in products to prevent featured being null

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ApplicationRecord
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :description, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
-  validates :featured, presence: true
+  validates :featured, presence: true, if: -> { featured.nil? }
 
   has_many :product_categories, dependent: :destroy
   has_many :categories, through: :product_categories
